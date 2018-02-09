33667f0fe30da0384b306cd182e8f1902dba13b9
yashjshah <yashjshah@yahoo.com>
2018-02-09 21:37:53 +0000

    Add Stocks Restrictions

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
    root 'welcome#index'
    get 'my_portfolio', to: "users#my_portfolio"
    get 'search_stocks', to: "stocks#search"
+   
+   resources :user_stocks, only: [:create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
