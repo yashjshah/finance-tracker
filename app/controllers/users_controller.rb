68a3e7bc8421ac78dc4412d190037d68bb00c92f
yashjshah <yashjshah@yahoo.com>
2018-02-11 21:54:55 +0000

    Add View profile feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,31 @@ class UsersController < ApplicationController
   def my_friends
     @friendships = current_user.friends
   end
+  
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = "You have entered an empty string"
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_users(@users)
+      flash.now[:danger] = "No users matches the search criteria" if @users.blank?
+    end
+    render partial: 'friends/result'
+  end
+  
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+    if current_user.save
+     flash[:success] = "Friend was succesfully added" 
+    else
+      flash[:danger] = "There was something wrong with the friend request"
+    end
+    redirect_to my_friends_path
+  end
+  
+  def show
+   @user  = User.find(params[:id]) 
+   @user_stocks = @user.stocks
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
    get 'my_portfolio', to: "users#my_portfolio"
    get 'search_stocks', to: "stocks#search"
    get 'my_friends', to: "users#my_friends"
-   
+   get 'search_friends', to: "users#search"
+   post 'add_friend', to: "users#add_friend"
    resources :user_stocks, only: [:create, :destroy]
    resources :users, only: [:show]
    resources :friendships
